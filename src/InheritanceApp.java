fa42413244f0d01e9e0c989bb4337179a0217165
egoing <>
2020-06-19 11:42:49 +0900

    3.overriding vs overloading

--- src/InheritanceApp.java
+++ src/InheritanceApp.java
@@ -2,6 +2,10 @@ class Cal{
     public int sum(int v1, int v2){
         return v1+v2;
     }
+    // Overloading
+    public int sum(int v1, int v2, int v3){
+        return v1+v2+v3;
+    }
 }
 class Cal3 extends Cal{
     public int minus(int v1, int v2){
@@ -12,11 +16,13 @@ class Cal3 extends Cal{
         System.out.println("Cal3!!");
         return v1+v2;
     }
+
 }
 public class InheritanceApp {
     public static void main(String[] args) {
         Cal c = new Cal();
         System.out.println(c.sum(2,1));
+        System.out.println(c.sum(2,1,1));
 
         Cal3 c3 = new Cal3();
         System.out.println(c3.sum(2,1));
